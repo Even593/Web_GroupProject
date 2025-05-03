e800fbf28dc48454f5488a8623132253a40b0bec
fish47 <fish47@126.com>
2025-05-03 19:01:48 +0800

    refactor(db): move database stuff to a module

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,20 @@
 import flask
-import flask_sqlalchemy
-import sqlalchemy.orm as sa_orm
 
-class __ModelBase(sa_orm.DeclarativeBase):
-    pass
-
-db = flask_sqlalchemy.SQLAlchemy(model_class=__ModelBase)
 api = flask.Blueprint("api", __name__, url_prefix="/api")
 
 def create_app() -> flask.Flask:
+    from . import db
     app = flask.Flask(__name__)
     app.config["SECRET_KEY"] = "dev"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-    db.init_app(app)
+    db.db.init_app(app)
 
     from . import user
     app.register_blueprint(api)
     app.register_blueprint(user.bp)
 
     with app.app_context():
-        db.create_all()
+        db.db.create_all()
 
     @app.route("/", endpoint="/")
     @user.route_to_login_if_required
