c7adb85bf85bce9055495b1cb2d3fa955cf9da8b
fish47 <fish47@126.com>
2025-05-05 17:04:43 +0800

    refactor(db): base model class type hints

--- app/db.py
+++ app/db.py
@@ -1,3 +1,5 @@
+import typing
+
 import flask_sqlalchemy
 import sqlalchemy.orm as sa_orm
 
@@ -5,6 +7,11 @@ class __BaseModel(sa_orm.DeclarativeBase):
     pass
 
 db = flask_sqlalchemy.SQLAlchemy(model_class=__BaseModel)
+
+if typing.TYPE_CHECKING:
+    class BaseModel(sa_orm.DeclarativeBase): ...
+else:
+    BaseModel = db.Model # pyright:ignore[reportUnreachable]
 
 class IdMixin:
     _id: sa_orm.Mapped[int] = sa_orm.mapped_column("id", primary_key=True)
