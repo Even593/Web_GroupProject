d150d8d97dca72fd2c9561ea4053cb5b299b32d8
fish47 <fish47@126.com>
2025-05-06 22:04:03 +0800

    refactor(db): declare primary key in the base model

--- app/db.py
+++ app/db.py
@@ -4,14 +4,12 @@ import flask_sqlalchemy
 import sqlalchemy.orm as sa_orm
 
 class __BaseModel(sa_orm.DeclarativeBase):
-    pass
+    _id: sa_orm.Mapped[int] = sa_orm.mapped_column("id", primary_key=True)
 
 db = flask_sqlalchemy.SQLAlchemy(model_class=__BaseModel)
 
 if typing.TYPE_CHECKING:
-    class BaseModel(sa_orm.DeclarativeBase): ...
+    class BaseModel(__BaseModel): ...
 else:
-    BaseModel = db.Model # pyright:ignore[reportUnreachable]
+    BaseModel = db.Model
 
-class IdMixin:
-    _id: sa_orm.Mapped[int] = sa_orm.mapped_column("id", primary_key=True)
