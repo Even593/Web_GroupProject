e4c339f3c286500d369d570746892c8ef65df299
fish47 <fish47@126.com>
2025-05-14 23:12:21 +0800

    refactor(user): encapsulate user instance access

--- app/util.py
+++ app/util.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import typing
+
 import flask
 
 def make_module_blueprints(name: str) -> tuple[flask.Blueprint, flask.Blueprint]:
@@ -7,3 +9,10 @@ def make_module_blueprints(name: str) -> tuple[flask.Blueprint, flask.Blueprint]
         prefix = "/" + name
         return flask.Blueprint(n, __name__, url_prefix=prefix)
     return __make(name), __make(name)
+
+def set_current_user(user):
+    flask.g.user = user
+
+def get_current_user():
+    from . import account
+    return typing.cast(account.Account, flask.g.user)
