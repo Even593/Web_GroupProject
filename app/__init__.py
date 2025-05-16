388f6699974b6be6a4ca92d6d180d85369e9c4bc
fish47 <fish47@126.com>
2025-05-16 16:34:35 +0800

    refactor(weight): generate report in memory

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,6 @@ def create_app() -> flask.Flask:
     from . import leaderboard
     api.register_blueprint(account.bp_api)
     app.register_blueprint(account.bp_view)
-    api.register_blueprint(weight.bp_api)
     app.register_blueprint(weight.bp_view)
     api.register_blueprint(workout.bp_api)
     app.register_blueprint(workout.bp_view)
