2e04445eb70c387f45100c3ea8721babf9a7ae0d
fish47 <fish47@126.com>
2025-05-16 22:22:23 +0800

    fix(test): unit test

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -20,7 +20,7 @@ class AppTestCase(unittest.TestCase):
     def test_home_status_code(self):
         # our root / is redirect to url..
         resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
